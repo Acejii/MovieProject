e42629061c1c9cc8307438a15a802b3a28e3d7f4
HoangThu <hoangthu7331@gmail.com>
2022-08-17 18:21:51 +0700

    header and footer

--- src/layouts/footer/Footer.js
+++ src/layouts/footer/Footer.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.scss";
+
+const footerInfo = [
+  {
+    title: "general",
+    content: [
+      { item: "VIP", path: "/" },
+      { item: "IMAX", path: "/" },
+      { item: "4DX", path: "/" },
+    ],
+  },
+  {
+    title: "who are we?",
+    content: [
+      { item: "About us", path: "/" },
+      { item: "FAQ", path: "/" },
+      { item: "Careers", path: "/" },
+    ],
+  },
+  {
+    title: "legal",
+    content: [
+      { item: "Privacy policies", path: "/" },
+      { item: "Terms and conditions", path: "/" },
+    ],
+  },
+  {
+    title: "contact us",
+    content: [
+      { item: "Feedback", path: "/" },
+      { item: "Sales & bulk booking enquiries", path: "/" },
+      { item: "Recover Email Confirmation", path: "/" },
+    ],
+  },
+];
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer__wrap container">
+        {footerInfo.map((item, index) => (
+          <div key={index} className="footer__item">
+            <h1 className="footer__item-title">{item.title}</h1>
+            {item.content.map((list, index) => (
+              <div key={index} className="footer__item-list">
+                <Link to={list.path} className="footer__item-item">
+                  {list.item}
+                </Link>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
