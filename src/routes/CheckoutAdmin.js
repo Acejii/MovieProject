d95cc0be0a5c47a4fc7e3ea21b58f90274a57af0
HoangThu <hoangthu7331@gmail.com>
2022-09-15 19:19:32 +0700

    add warningpage

--- src/routes/CheckoutAdmin.js
+++ src/routes/CheckoutAdmin.js
@@ -1,14 +1,19 @@
 import Page404 from "modules/Page404";
+import WarningPage from "modules/ResponsiveWarningPage/WarningPage";
 import { useSelector } from "react-redux";
 
 const CheckoutAdmin = ({ children }) => {
   const { user } = useSelector((state) => state.auth);
 
-  if (user && user.maLoaiNguoiDung === "QuanTri") {
-    return children;
+  if (user && user.maLoaiNguoiDung !== "QuanTri") {
+    return <Page404 />;
   }
 
-  return <Page404 />;
+  if (window.innerWidth < 1200) {
+    return <WarningPage />;
+  }
+
+  return children;
 };
 
 export default CheckoutAdmin;
